9b1babe855beac12d961e2896256107b78aaca84
Tyler Rick <tyler@k3integrations.com>
2010-11-30 16:28:13 -0800

    logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Temporary hack until we have an authentication engine
+  def logged_in?
+    true
+  end
+  helper_method :logged_in?
 end
