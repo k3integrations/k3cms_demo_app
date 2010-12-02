126a32ae44633161ffc7ad98f9e32b19be78b141
Tyler Rick <tyler@k3integrations.com>
2010-12-01 17:35:52 -0800

    Set test_framework to :rspec to so rails generate will generate specs

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,9 @@ module FrameworkDemo
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
