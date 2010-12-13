6e0a5c57d85b541703678a735abec7959c0d4175
Tyler Rick <tyler@k3integrations.com>
2010-12-13 14:23:15 -0800

    Apparently we need to specify helpers from engines/plugins explicitly? (added this so we could use the edit_mode? helper from ribbon)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # TODO: We should not have to list them all individually
+  helper 'k3/ribbon/ribbon'
+
   # Temporary hack until we have an authentication engine
   def logged_in?
     true
