eda0418b7a7ca8a765136a89cc0ac0d3cc49e251
Tyler Rick <tyler@k3integrations.com>
2011-01-31 20:17:29 -0800

    Revert "Fix annoying devise error"
    This reverts commit 76b6d44a98650e207375c1e7eb7e8254b9456ee9.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FrameworkDemo::Application.config.secret_token = '9fc80b8435b159edcdee98ffed08d43f9f085be6cd67bde77f4143a2e43fe51f6b3564f5edffd6d5b910f89fee05af1e35c44783b18e7198f8891a142eb225ch'
+FrameworkDemo::Application.config.secret_token = '9fc80b8435b159edcdee98ffed08d43f9f085be6cd67bde77f4143a2e43fe51f6b3564f5edffd6d5b910f89fee05af1e35c44783b18e7198f8891a142eb225cg'
