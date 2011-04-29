faa87d263514091be61cdf0f9522e7c22157d721
Tyler Rick <tyler@k3integrations.com>
2011-04-29 08:10:02 -0700

    Override after_sign_in_path_for, after_sign_out_path_for in ApplicationController to avoid getting this error: NameError (undefined local variable or method `root_path' for #<Devise::SessionsController:0x000000047137f8>)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # Override lib/devise/controllers/helpers.rb, since that references root_path
+  def after_sign_in_path_for(resource_or_scope)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    home_path = "#{scope}_root_path"
+    '/'
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    '/'
+  end
+
   # Not necessary the way we're doing things, but allows the file to be reloaded during development
-  helper K3cms::S3Podcast::S3PodcastHelper
   helper K3cms::InlineEditor::InlineEditorHelper
 end
