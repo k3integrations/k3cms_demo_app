4e3798c2804100ac586615fa81ec9cf8f0deabf0
Tyler Rick <tyler@k3integrations.com>
2011-05-11 17:21:01 -0700

    Updated to use new names, config.k3cms.s3_podcast.*

--- config/initializers/k3cms_s3_podcast.rb
+++ config/initializers/k3cms_s3_podcast.rb
@@ -1,12 +1,12 @@
 Rails.application.class_eval do
-  config.k3cms_s3_podcast_video_tag_options = {
+  config.k3cms.s3_podcast.video_tag_options = {
     :width => 326, :height => 269
   }
-  config.k3cms_s3_podcast_pagination = {
+  config.k3cms.s3_podcast.pagination = {
     :per_row => 3, :per_page => 12
   }
-  config.k3cms_s3_index_view = :tiles
-  config.k3cms_s3_show_view  = :page
+  config.k3cms.s3_podcast.index_view = :tiles
+  config.k3cms.s3_podcast.show_view  = :page
 
   local_file = __FILE__.gsub(/\.rb$/, '.local.rb')
   if File.exists?(local_file)
